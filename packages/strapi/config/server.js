0e2aaf6c0fc2af89a3a53e6716078a361153649d
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-08-07 23:50:32 -0300

    create strapi

---
+++ packages/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6d31dace56e90d98a6e7d766b710532d'),
+    },
+  },
+});
