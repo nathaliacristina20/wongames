1a4ebda00cc792aa79032c8ebadbf5934c2d863b
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-03 20:07:21 -0300

    feat(client): add logo without text for mobile

--- packages/client/src/components/Logo/test.tsx
+++ packages/client/src/components/Logo/test.tsx
@@ -3,6 +3,8 @@ import { screen } from '@testing-library/react';
 import Logo from '.';
 import { renderWithTheme } from 'utils/tests/helpers';
 
+import 'jest-styled-components';
+
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
     renderWithTheme(<Logo />);
@@ -30,5 +32,16 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '11rem'
     });
+  });
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    );
   });
 });
