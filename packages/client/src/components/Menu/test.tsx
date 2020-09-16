4d9feedf9df9ce499664c5f423f62daa204e9e90
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-16 20:22:50 -0300

    feat(client): create styles for full menu

--- packages/client/src/components/Menu/test.tsx
+++ packages/client/src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import Menu from '.';
@@ -11,5 +11,24 @@ describe('<Menu />', () => {
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument();
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument();
+  });
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />);
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true });
+
+    // verifica se o menu ta escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
+
+    // clica no botao de abrir o menu e verifica se abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i));
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false');
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 });
+
+    // clica no botao de abrir o menu e verifica se abriu
+    fireEvent.click(screen.getByLabelText(/close menu/i));
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
   });
 });
