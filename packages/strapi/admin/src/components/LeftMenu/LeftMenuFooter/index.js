c0fec4961b4e9193d23b646f858f200a7e19b0b3
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-16 20:43:01 -0300

    chore(client): update strapi for version 3.1.5

--- packages/strapi/admin/src/components/LeftMenu/LeftMenuFooter/index.js
+++ packages/strapi/admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -5,22 +5,13 @@
  */
 
 import React from 'react';
-import { defineMessages, FormattedMessage } from 'react-intl';
-
 import Wrapper from './Wrapper';
-import messages from './messages.json';
-
-defineMessages(messages);
 
 function LeftMenuFooter() {
   return (
     <Wrapper>
       <div className="poweredBy">
-        <FormattedMessage
-          id={messages.poweredBy.id}
-          defaultMessage={messages.poweredBy.defaultMessage}
-          key="poweredBy"
-        />
+        <span>Mantido por</span>&nbspa;
         <a key="website" href="https://reactavancado.com.br" target="_blank" rel="noopener noreferrer">
           React Avançado
         </a>
