1aef8ef3310ac976a6539e5c361bd7f140cefc12
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-16 22:31:04 -0300

    feat(client): rule for menu when user logged

--- packages/client/src/components/Menu/index.tsx
+++ packages/client/src/components/Menu/index.tsx
@@ -9,7 +9,10 @@ import Button from 'components/Button';
 
 import * as S from './styles';
 
-const Menu = () => {
+export type MenuProps = {
+  username?: string;
+};
+const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -33,17 +36,28 @@ const Menu = () => {
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
         </S.MenuNav>
 
-        <S.RegisterBox>
-          <Button fullWidth size="large">
-            Log in now
-          </Button>
-          <span>or</span>
-          <S.CreateAccount href="#" title="Sign In">
-            Sign Up
-          </S.CreateAccount>
-        </S.RegisterBox>
+        {!username && (
+          <>
+            <S.RegisterBox>
+              <Button fullWidth size="large">
+                Log in now
+              </Button>
+              <span>or</span>
+              <S.CreateAccount href="#" title="Sign In">
+                Sign Up
+              </S.CreateAccount>
+            </S.RegisterBox>
+          </>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   );
