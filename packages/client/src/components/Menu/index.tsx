b42db3b20193b74ee67154069b554cd71cb0feb1
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-16 22:53:05 -0300

    feat(client): ajustmentment menu for desktop

--- packages/client/src/components/Menu/index.tsx
+++ packages/client/src/components/Menu/index.tsx
@@ -4,25 +4,37 @@ import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outline
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
-import Logo from 'components/Logo';
 import Button from 'components/Button';
-
+import Logo from 'components/Logo';
+import MediaMatch from 'components/MediaMatch';
 import * as S from './styles';
 
 export type MenuProps = {
   username?: string;
 };
+
 const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <S.Wrapper>
-      <S.IconWrapper onClick={() => setIsOpen(true)}>
-        <MenuIcon aria-label="Open Menu" />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
+
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
@@ -30,7 +42,13 @@ const Menu = ({ username }: MenuProps) => {
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
+
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
@@ -46,17 +64,15 @@ const Menu = ({ username }: MenuProps) => {
         </S.MenuNav>
 
         {!username && (
-          <>
-            <S.RegisterBox>
-              <Button fullWidth size="large">
-                Log in now
-              </Button>
-              <span>or</span>
-              <S.CreateAccount href="#" title="Sign In">
-                Sign Up
-              </S.CreateAccount>
-            </S.RegisterBox>
-          </>
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
         )}
       </S.MenuFull>
     </S.Wrapper>
--- packages/client/src/components/Menu/styles.ts
+++ packages/client/src/components/Menu/styles.ts
@@ -30,6 +30,7 @@ export const IconWrapper = styled.div`
 export const MenuGroup = styled.div`
   ${({ theme }) => css`
     display: flex;
+    align-items: center;
     flex-grow: 1;
     justify-content: flex-end;
     > div {
@@ -38,11 +39,18 @@ export const MenuGroup = styled.div`
   `}
 `;
 
-export const MenuNav = styled.div``;
+export const MenuNav = styled.div`
+  ${({ theme }) => css`
+    ${media.greaterThan('medium')`
+			margin-left: ${theme.spacings.small};
+		`}
+  `}
+`;
 
 export const MenuLink = styled.a`
   ${({ theme }) => css`
     position: relative;
+    color: ${theme.colors.white};
     font-size: ${theme.font.sizes.medium};
     margin: 0.3rem ${theme.spacings.small} 0;
     text-decoration: none;
