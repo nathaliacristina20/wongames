1f5cc0613ef9b0699bc3c9e4ac9033983082c63e
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-16 22:06:26 -0300

    feat(client): styles menu

--- packages/client/src/components/Menu/index.tsx
+++ packages/client/src/components/Menu/index.tsx
@@ -5,6 +5,8 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 import Logo from 'components/Logo';
+import Button from 'components/Button';
+
 import * as S from './styles';
 
 const Menu = () => {
@@ -27,10 +29,21 @@ const Menu = () => {
         </S.IconWrapper>
       </S.MenuGroup>
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
-        <CloseIcon
-          aria-label="Close Menu"
-          onClick={() => setIsOpen(false)}
-        ></CloseIcon>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign In">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   );
