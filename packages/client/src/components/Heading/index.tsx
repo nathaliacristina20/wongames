dae93410534b7cdc8efd4c7e640159c52345199c
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-28 21:12:26 -0300

    feat(client): add lineColors for heading component

--- packages/client/src/components/Heading/index.tsx
+++ packages/client/src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as S from './styles';
 
+export type LineColors = 'primary' | 'secondary';
+
 export type HeadingProps = {
   children: React.ReactNode;
   color?: 'white' | 'black';
   lineLeft?: boolean;
   lineBottom?: boolean;
+  lineColor?: LineColors;
+  size?: 'small' | 'medium';
 };
 
 const Heading = ({
   children,
   color = 'white',
+  lineColor = 'primary',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+    lineColor={lineColor}
+  >
     {children}
   </S.Wrapper>
 );
