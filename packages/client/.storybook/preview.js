f05b78aa5547168a3a314bfe40b50c8e484f2c28
Nathalia <nathaliacristina20@hotmail.com>
2021-03-03 19:34:53 -0300

    feat(client): add background customized storybook"

--- packages/client/.storybook/preview.js
+++ packages/client/.storybook/preview.js
@@ -1,6 +1,22 @@
-import GlobalStyles from 'styles/global'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
+
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+};
 
 export const decorators = [
   (Story) => (
@@ -9,4 +25,4 @@ export const decorators = [
       <Story />
     </ThemeProvider>
   )
-]
+];
